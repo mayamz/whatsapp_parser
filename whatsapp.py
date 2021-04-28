336a6412726e45c96f733985c5d7579703d2c870
mayamz <mayametz@gmail.com>
2021-04-28 20:12:08 +0300

    fixed bug in curse counter

--- whatsapp.py
+++ whatsapp.py
@@ -107,17 +107,39 @@ def count_questions(df, regex=True):
 
 def count_curses(df):
     counter = pd.DataFrame()
-    curse_list = ["פאק", "שיט", "סעמק", "זונה", "דמט", "דאם", "דאמ", "לעזאזל", "זין"]
+    curse_list = ["פאק", "שיט", "סעמק", "זונה", "דמט", "דאם", "דאמ", "לעזאזל", "זין", "רבאק"]
     for curse in curse_list:
+        curse_row = pd.DataFrame()
         # generate the regex that allows letter repetitions
         curse_regex = ""
         for letter in curse:
             curse_regex = curse_regex+letter+"+"
 
-        counter[curse] = count_word(df,curse_regex).iloc[:,1]
+        curse_row[curse] = count_word(df,curse_regex).iloc[:,1]
+        counter = pd.concat([counter,curse_row],axis=1)
     counter["total"] = counter.sum(axis=1)
     counter = counter.fillna(0)
     counter = counter.transpose()
+    return counter
+
+def counter_by_user(df):
+    ''' all the counters in one df
+        currently: messages, words, hhh, emoji, questions
+        all by authors
+        '''
+    counter = pd.DataFrame()
+    counter["messages"] = df.groupby("author").count().iloc[:,1]
+    counter["words"] = None
+    for author in Message.authors:
+        authors_words = " ".join(df[df['author'] == author]["text"]).split()  # Filter here if you want to remove "yes" and whatever
+        counter.loc[author,"words"] = len(authors_words)
+
+    counter["hhh"] = count_haha(df).iloc[:,1]
+    counter["emoji"] = count_emoji(df).iloc[:,1]
+    counter["questions"] = count_questions(df).iloc[:,1]
+    counter["keilu"] = count_word(df,"כאילו").iloc[:,1]
+    counter = counter.transpose()
+
     return counter
 
 def clean_text(text):
@@ -146,6 +168,11 @@ def clean_text(text):
                               "]+", flags=re.UNICODE)
     clean_text = weirdPatterns.sub(r'', text)
     return (clean_text)
+
+def remove_punctuation(df):
+    punctuations = re.compile('[!"#$%&\'()*+\-./:;<=>?\[\]^_`{|}~]')
+    df["text"] = df["text"].replace(punctuations,"",regex=True)
+    return df
 
 def main(path,name):
     with open(path+name+".txt", encoding="utf-8") as f:
